4c266bb50ea072a89ef5a403f6de7055fe319d29
Dinara Stepina <setare497@gmail.com>
2025-03-31 16:26:30 +0300

    removed necessary implementations for local notifications

--- app/src/main/java/ru/yandexpraktikum/blechat/presentation/notifications/NotificationsHelperImpl.kt
+++ app/src/main/java/ru/yandexpraktikum/blechat/presentation/notifications/NotificationsHelperImpl.kt
@@ -1,13 +1,8 @@
 package ru.yandexpraktikum.blechat.presentation.notifications
 
 import android.annotation.SuppressLint
-import android.app.NotificationManager
 import android.content.Context
-import androidx.core.app.NotificationChannelCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import dagger.hilt.android.qualifiers.ApplicationContext
-import ru.yandexpraktikum.blechat.R
 import javax.inject.Inject
 
 private const val CHANNEL_ID = "channel_id"
@@ -16,18 +11,10 @@ private const val NOTIFICATION_ID = 1234
 class NotificationsHelperImpl
 @Inject constructor(
     @ApplicationContext private val context: Context,
-    private val notificationManager: NotificationManagerCompat,
 ): NotificationsHelper {
 
     @SuppressLint("MissingPermission")
     override fun notifyOnMessageReceived(title: String, message: String) {
-        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setContentTitle(title)
-            .setContentText(message)
-            .setSmallIcon(R.drawable.bluetooth_ic)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-
-        notificationManager.notify(NOTIFICATION_ID, notification)
+        TODO()
     }
 }
